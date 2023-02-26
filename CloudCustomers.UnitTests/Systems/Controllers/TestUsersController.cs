3dceb56e5d442bb7b9175425cf0dfd5ab5984ace
lzlocki <lzlocki@poczta.fm>
2023-02-26 08:50:38 +0100

    Set up unit tests

---
+++ CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -0,0 +1,16 @@
+namespace CloudCustomers.UnitTests.Systemsabstract.Controllers;
+
+public class TestUsersController
+{
+    [Fact]
+    public void Get_OnSuccess_ReturnsStatusCode200()
+    {
+        // Ararnge
+
+
+        // Act
+
+        
+        // Assert
+    }
+}
