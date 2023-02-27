09ce2ed24c068614d326e16d4c4e4a3b3e1fb7c5
lzlocki <lzlocki@poczta.fm>
2023-02-27 22:29:18 +0100

    Add fixture to generate users

---
+++ CloudCustomers.UnitTests/Fixtures/UsersFixture.cs
@@ -0,0 +1,36 @@
+﻿using CloudCustomers.API.Models;
+using System.Xml.Linq;
+
+namespace CloudCustomers.UnitTests.Fixtures
+{
+    public static class UsersFixture
+    {
+        public static List<User> GetTestUsers() => new() {
+                new User
+                {
+                    Id = 1,
+                    Name = "Jane",
+                    Email = "jane@ab.ab",
+                    Address = new Address
+                    {
+                        Street = "54 streer name",
+                        City = "city_name",
+                        ZipCode = "451078"
+                    }
+                },
+                 new User
+                 {
+                    Id = 1,
+                    Name = "Kane",
+                    Email = "Kane@ab.ab",
+                    Address = new Address
+                    {
+                        Street = "66 streer name",
+                        City = "city_name2",
+                        ZipCode = "11111"
+                    }
+                 }
+        };
+
+    }
+}
