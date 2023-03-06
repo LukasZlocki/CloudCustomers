04d1c2a6a45808202b325fddf0d9d895773990c4
lzlocki <lzlocki@poczta.fm>
2023-03-06 21:51:21 +0100

    Add fake db data as USersFixture

--- CloudCustomers.UnitTests/Fixtures/UsersFixture.cs
+++ CloudCustomers.UnitTests/Fixtures/UsersFixture.cs
@@ -8,26 +8,35 @@ namespace CloudCustomers.UnitTests.Fixtures
         public static List<User> GetTestUsers() => new() {
                 new User
                 {
-                    Id = 1,
-                    Name = "Jane",
-                    Email = "jane@ab.ab",
+                    Name = "Test User 1",
+                    Email = "test.user.1@ab.ab",
                     Address = new Address
                     {
                         Street = "54 streer name",
-                        City = "city_name",
-                        ZipCode = "451078"
+                        City = "city_name1",
+                        ZipCode = "111111"
                     }
                 },
                  new User
                  {
-                    Id = 1,
-                    Name = "Kane",
-                    Email = "Kane@ab.ab",
+                    Name = "Test User 2",
+                    Email = "test.user.2@ab.ab",
                     Address = new Address
                     {
                         Street = "66 streer name",
                         City = "city_name2",
-                        ZipCode = "11111"
+                        ZipCode = "222222"
+                    }
+                 },
+                 new User
+                 {
+                    Name = "Test User 2",
+                    Email = "test.user.3@ab.ab",
+                    Address = new Address
+                    {
+                        Street = "66 streer name",
+                        City = "city_name3",
+                        ZipCode = "333333"
                     }
                  }
         };
