afd55221ffdb3e3b75f116aabee371a36cf52ea8
lzlocki <lzlocki@poczta.fm>
2023-03-07 22:27:42 +0100

    Add new testing method

--- CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
+++ CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -3,6 +3,7 @@ using CloudCustomers.API.Models;
 using CloudCustomers.API.Services;
 using CloudCustomers.UnitTests.Fixtures;
 using CloudCustomers.UnitTests.Help;
+using FluentAssertions;
 using Moq;
 using Moq.Protected;
 using Xunit;
@@ -31,6 +32,25 @@ namespace CloudCustomers.UnitTests.Systems.Services
                 ItExpr.IsAny<CancellationToken>()
                 );
         }
+
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_ReturnsListOfUsers()
+        {
+            // Arrange
+            var expectedResponse = UsersFixture.GetTestUsers();
+            var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
+            var httpClient = new HttpClient(handlerMock.Object);
+            var sut = new UsersService(httpClient);
+
+            // Act
+            var result = await sut.GetAllUsers();
+
+            // Assert
+            result.Should().BeOfType<List<User>>();
+
+
+        }
+
 
     }
 }
