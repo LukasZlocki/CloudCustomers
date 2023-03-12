2ca187a12bbc4a2caf3e66343513079bef8c8777
lzlocki <lzlocki@poczta.fm>
2023-03-12 09:48:51 +0100

    Add configuration to each methods

--- CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
+++ CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -21,7 +21,13 @@ namespace CloudCustomers.UnitTests.Systems.Services
             var expectedResponse = UsersFixture.GetTestUsers();
             var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
             var httpClient = new HttpClient(handlerMock.Object);
-            var sut = new UsersService(httpClient);
+            var endpoint = "https://exapmples.com";
+            var config = Options.Create(
+            new UsersApiOptions
+            {
+                Endpoint = endpoint
+            });
+            var sut = new UsersService(httpClient, config);
 
             // Act
             await sut.GetAllUsers();
@@ -42,7 +48,13 @@ namespace CloudCustomers.UnitTests.Systems.Services
             var expectedResponse = UsersFixture.GetTestUsers();
             var handlerMock = MockHttpMessageHandler<User>.SetupReturn404();
             var httpClient = new HttpClient(handlerMock.Object);
-            var sut = new UsersService(httpClient);
+            var endpoint = "https://exapmples.com";
+            var config = Options.Create(
+            new UsersApiOptions
+            {
+                Endpoint = endpoint
+            });
+            var sut = new UsersService(httpClient, config);
 
             // Act
             var result = await sut.GetAllUsers();
@@ -58,7 +70,13 @@ namespace CloudCustomers.UnitTests.Systems.Services
             var expectedResponse = UsersFixture.GetTestUsers();
             var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
             var httpClient = new HttpClient(handlerMock.Object);
-            var sut = new UsersService(httpClient);
+            var endpoint = "https://exapmples.com";
+            var config = Options.Create(
+            new UsersApiOptions
+            {
+                Endpoint = endpoint
+            });
+            var sut = new UsersService(httpClient, config);
 
             // Act
             var result = await sut.GetAllUsers();
