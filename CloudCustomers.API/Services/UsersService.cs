cf210dc7c9d36a004ce6119ab28ce1463a8c4c14
lzlocki <lzlocki@poczta.fm>
2023-03-12 09:39:07 +0100

    Add and configure test

--- CloudCustomers.API/Services/UsersService.cs
+++ CloudCustomers.API/Services/UsersService.cs
@@ -1,4 +1,6 @@
-﻿using CloudCustomers.API.Models;
+﻿using CloudCustomers.API.Config;
+using CloudCustomers.API.Models;
+using Microsoft.Extensions.Options;
 
 namespace CloudCustomers.API.Services
 {
@@ -11,9 +13,12 @@ namespace CloudCustomers.API.Services
     public class UsersService : IUsersService
     {
         private readonly HttpClient _httpClient;
+        private readonly UsersApiOptions _apiConfig;
 
-        public UsersService(HttpClient httpClient) {
+        public UsersService(
+            HttpClient httpClient, IOptions<UsersApiOptions> apiConfig) {
             _httpClient = httpClient;
+            _apiConfig = apiConfig.Value;
         }
 
         public async Task<List<User>> GetAllUsers()
--- CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
+++ CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -4,6 +4,7 @@ using CloudCustomers.API.Services;
 using CloudCustomers.UnitTests.Fixtures;
 using CloudCustomers.UnitTests.Help;
 using FluentAssertions;
+using Microsoft.Extensions.Options;
 using Moq;
 using Moq.Protected;
 using Xunit;
@@ -57,6 +58,30 @@ namespace CloudCustomers.UnitTests.Systems.Services
             var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse);
             var httpClient = new HttpClient(handlerMock.Object);
             var sut = new UsersService(httpClient);
+
+            // Act
+            var result = await sut.GetAllUsers();
+
+            // Assert
+            result.Count.Should().Be(expectedResponse.Count);
+        }
+
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_InnvokesConfiguredExternalUrl()
+        {
+            // Arrange
+            var expectedResponse = UsersFixture.GetTestUsers();
+            var endpoint = "https://exapmples.com/users";
+            var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetResourceList(expectedResponse, endpoint);
+
+            var httpClient = new HttpClient(handlerMock.Object);
+            var config = Options.Create(
+                new UsersApiOptions 
+                { 
+                    Endpoint = endpoint 
+                });
+
+            var sut = new UsersService(httpClient, config);
 
             // Act
             var result = await sut.GetAllUsers();
